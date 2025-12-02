7d5d00cb1dd0429ade4ff53209c85542f18101ae
jordanw-hub <229931303+jordanw-hub@users.noreply.github.com>
2025-12-02 01:42:29 -1000

    Add new categories to test event

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -86,6 +86,10 @@ async function main() {
           location: event.location,
           eventSize,
           categories,
+          categoriesNew: {
+            connect:
+              event.categoriesNew,
+          },
           createdById: organizerId, // Link to organizer user
         },
       });
